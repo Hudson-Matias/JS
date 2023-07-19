a4f57ef84cbbb900788d86e7d5563dfd070d8e06
Hudson Matias <hudson.um@gmail.com>
2023-07-19 10:09:12 -0300

    Laços de Repetição

---
+++ Aula5/Aula5.js
@@ -0,0 +1,10 @@
+var x = 0
+while(x < 6){
+    console.log('Tudo bem?')
+    x++
+}
+
+do{
+    console.log('Mó gatinha')
+    x++
+}while(x<6)
---
+++ Aula5/node_db8e25869f3db.tmp.js
@@ -0,0 +1,13 @@
+var x = 0
+while(x < 6){
+    console.log('Tudo bem?')
+    x++
+}
+
+for(x=0;x<6;x++){
+    console.log('Cê é mó gay')
+}
+
+do{
+    console.log('Besta')
+} while(x<6,x++)
