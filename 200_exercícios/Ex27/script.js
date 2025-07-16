6b626580a79ddb678a68a1e052bb1a0ea78ccb24
Hudson Matias <116506548+Hudson-Matias@users.noreply.github.com>
2025-07-16 20:20:58 +0000

    Treinando 200 exercícios de lógica com JS

--- 200_exercícios/Ex27/script.js
+++ 200_exercícios/Ex27/script.js
@@ -2,6 +2,9 @@ let numeroInit = 0;
 let numeroEnd = 20;
 
 
-// do{
-//     if ()
-// } while (numeroInit < numeroEnd)
+do{
+    if (numeroInit % 2 != 0){
+        console.log(`Números ímpares: ${numeroInit}`);
+    }
+    numeroInit++;
+} while (numeroInit < numeroEnd);
---
+++ 200_exercícios/Ex28/script.js
@@ -0,0 +1,4 @@
+for(let x = 1; x < 11; x++){
+    var multiplica = 5 * x;
+    console.log(`Multiplicação do 5 * ${x} = ${multiplica}`);
+}
